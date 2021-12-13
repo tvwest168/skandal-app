f4bf9f64f14b9008efcb21f74fa571793d712d9f
West <tvwest168@gmail.com>
2021-12-14 02:36:17 +0700

    Change Ads Link
    Change Ads Link

--- src/component/Contents/RecomendedVideo.js
+++ src/component/Contents/RecomendedVideo.js
@@ -58,7 +58,7 @@ function RecomendedVideo({title,categoryMain,typeCategoryMain}){
             // code block
              configPosts = {
               method: 'GET',
-              url: 'https://txmo.studioskandal.com/?mod=list_movies_all'
+              url: 'https://txmo.studioskandal.com/?mod=list_movies_all&limit=100'
             };
           
              page = new  URLSearchParams(location.hash.replace('#/?','')).get('p');
--- src/component/Contents/ads_bottom.js
+++ src/component/Contents/ads_bottom.js
@@ -4,8 +4,8 @@ import './ads_bottom.css';
 function ads_bottom() {
     return (
         <div className="recomendedvideo_bottom_ads">
-           <div className="bottom_ads_item " key="1"><a href="http://50.28.102.195/register" rel="noreferrer" target="_blank"><img alt="mkvgames" src="https://play.studioskandal.com/ipfs/QmPB4YLXmJ2zNm2ahLyT4yqvb3yxnGmhMQ66JYWw6Se4w1"></img></a></div>
-            <div className="bottom_ads_item " key="2"><a href="http://indowins.bond" rel="noreferrer" target="_blank"><img alt="indowins" src="https://play.studioskandal.com/ipfs/QmNyvBwPJRjBmoh7BLAa4qkBAoFymVzqMzVmcj69eroqYc"></img></a></div>
+           <div className="bottom_ads_item " key="1"><a href="http://50.28.102.195/register.php?ref=FD1B18D28E" rel="noreferrer" target="_blank"><img alt="mkvgames" src="https://play.studioskandal.com/ipfs/QmPB4YLXmJ2zNm2ahLyT4yqvb3yxnGmhMQ66JYWw6Se4w1"></img></a></div>
+            <div className="bottom_ads_item " key="2"><a href="http://216.83.47.53/Main.aspx?ref=275BE8" rel="noreferrer" target="_blank"><img alt="indowins" src="https://play.studioskandal.com/ipfs/QmNyvBwPJRjBmoh7BLAa4qkBAoFymVzqMzVmcj69eroqYc"></img></a></div>
          </div>
     )
 }
