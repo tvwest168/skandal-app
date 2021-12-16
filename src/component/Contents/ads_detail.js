9754c79b0dd174ee1eb29b8d63301a306ddc7663
West <tvwest168@gmail.com>
2021-12-16 12:32:38 +0700

    Update Banner Tengah

--- src/component/Contents/ads_detail.js
+++ src/component/Contents/ads_detail.js
@@ -4,8 +4,8 @@ import './ads_detail.css';
 function ads_detail() {
     return (
         <div className="recomendedvideo_detail_ads">
-        <div className="detail_ads_item " key="1"><a href="http://studioskandal.com" rel="noreferrer" target="_blank"><img alt="skandal1" src="https://play.studioskandal.com/ipfs/QmY3JzT5UCcQbseDFa9pFVvteS3sWCGu9vZhA1NdFnyBNW"></img></a></div>
-         <div className="detail_ads_item " key="2"><a href="http://studioskandal.com" rel="noreferrer" target="_blank"><img alt="skandal2" src="https://play.studioskandal.com/ipfs/QmfLTF1CDwBNeCWvdwQDDfALLY4gLRn1ggBQL2kHNNjvkb"></img></a></div>
+        <div className="detail_ads_item " key="1"><a href="https://157.230.195.128/?ref=studios" rel="noreferrer" target="_blank"><img alt="skandal1" src="https://play.studioskandal.com/ipfs/QmVauTezAH5aW5bhRPSb353typ4yp3wUfjsgan3UsgTpZD"></img></a></div>
+         <div className="detail_ads_item " key="2"><a href="http://216.83.47.53/?ref=275BE8" rel="noreferrer" target="_blank"><img alt="skandal2" src="https://play.studioskandal.com/ipfs/QmQqN1TiDq5NJchvXtcxrdhgFPxhB2TVibdnrwbfx8Jvce"></img></a></div>
       </div>
     )
 }
