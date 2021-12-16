8f8e5cfa24a759042f0a81d837dbf2cd799ecc92
West <tvwest168@gmail.com>
2021-12-16 13:12:40 +0700

    Fix Banner size

--- src/component/Contents/ads_detail.js
+++ src/component/Contents/ads_detail.js
@@ -5,7 +5,7 @@ function ads_detail() {
     return (
         <div className="recomendedvideo_detail_ads">
         <div className="detail_ads_item " key="1"><a href="https://157.230.195.128/?ref=studios" rel="noreferrer" target="_blank"><img alt="skandal1" src="https://play.studioskandal.com/ipfs/QmVauTezAH5aW5bhRPSb353typ4yp3wUfjsgan3UsgTpZD"></img></a></div>
-         <div className="detail_ads_item " key="2"><a href="http://216.83.47.53/?ref=275BE8" rel="noreferrer" target="_blank"><img alt="skandal2" src="https://play.studioskandal.com/ipfs/QmQqN1TiDq5NJchvXtcxrdhgFPxhB2TVibdnrwbfx8Jvce"></img></a></div>
+         <div className="detail_ads_item detail_item2"  key="2"><a href="http://216.83.47.53/?ref=275BE8" rel="noreferrer" target="_blank"><img alt="skandal2" src="https://play.studioskandal.com/ipfs/QmQqN1TiDq5NJchvXtcxrdhgFPxhB2TVibdnrwbfx8Jvce"></img></a></div>
       </div>
     )
 }
