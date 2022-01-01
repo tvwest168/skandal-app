2cd65378dfe469e5cc42e5e043d80c075cc468ba
West <tvwest168@gmail.com>
2022-01-01 17:22:51 +0700

    update jendralpoker
    jendralpoker banner

--- src/component/Contents/ads_bottom.js
+++ src/component/Contents/ads_bottom.js
@@ -5,7 +5,7 @@ function ads_bottom() {
     return (
         <div className="recomendedvideo_bottom_ads">
            <div className="bottom_ads_item " key="1"><a href="http://50.28.102.195/register.php?ref=FD1B18D28E" rel="noreferrer" target="_blank"><img alt="mkvgames" src="https://play.studioskandal.com/ipfs/QmPB4YLXmJ2zNm2ahLyT4yqvb3yxnGmhMQ66JYWw6Se4w1"></img></a></div>
-            <div className="bottom_ads_item " key="2"><a href="http://216.83.47.53/Main.aspx?ref=275BE8" rel="noreferrer" target="_blank"><img alt="indowins" src="https://play.studioskandal.com/ipfs/QmNyvBwPJRjBmoh7BLAa4qkBAoFymVzqMzVmcj69eroqYc"></img></a></div>
+            <div className="bottom_ads_item " key="2"><a href="http://180.215.200.120/?ref=studioskandal" rel="noreferrer" target="_blank"><img alt="indowins" src="https://play.studioskandal.com/ipfs/QmYaQSoi6xRQyf9ZKE1p36fWCAmC1C3nY7XxUqutK9rkyy"></img></a></div>
          </div>
     )
 }
