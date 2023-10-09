c0ab598b57d4f13b551d198f01a6112ac0cc80f9
tvwest168 <tvwest168@gmail.com>
2023-10-09 15:09:59 +0700

    update jp

--- src/component/Contents/ads_bottom.js
+++ src/component/Contents/ads_bottom.js
@@ -5,7 +5,7 @@ function ads_bottom() {
     return (
         <div className="recomendedvideo_bottom_ads">
            <div className="bottom_ads_item " key="1"><a href="https://mkvgames88.info/register.php?ref=FD1B18D28E" rel="noreferrer" target="_blank"><img alt="mkvgames" src="http://100.26.60.62/ipfs/QmPB4YLXmJ2zNm2ahLyT4yqvb3yxnGmhMQ66JYWw6Se4w1"></img></a></div>
-            <div className="bottom_ads_item " key="2"><a href="https://jendralpoker.store/?ref=studioskandal" rel="noreferrer" target="_blank"><img alt="jendralpoker" src="http://100.26.60.62/ipfs/QmPYAos9k3bMfG8yYv4STxtYDLtzYLHyuUddre6n8TUqio"></img></a></div>
+            <div className="bottom_ads_item " key="2"><a href="https://jp888.store/?ref=studioskandal" rel="noreferrer" target="_blank"><img alt="jendralpoker" src="http://100.26.60.62/ipfs/QmPYAos9k3bMfG8yYv4STxtYDLtzYLHyuUddre6n8TUqio"></img></a></div>
          </div>
     )
 }
