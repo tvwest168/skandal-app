8dc100085abf3a3a36d8a53bb833aa3e914c6382
tvwest168 <tvwest168@gmail.com>
2023-10-25 15:01:19 +0700

    update banner 25102023

--- src/component/Contents/ads_detail.js
+++ src/component/Contents/ads_detail.js
@@ -4,8 +4,8 @@ import './ads_detail.css';
 function ads_detail() {
     return (
         <div className="recomendedvideo_detail_ads">
-        <div className="detail_ads_item " key="1"><a href="https://jp888.store/?ref=studioskandal" rel="noreferrer" target="_blank"><img alt="jendralpoker" src="http://100.26.60.62/ipfs/QmPYAos9k3bMfG8yYv4STxtYDLtzYLHyuUddre6n8TUqio"></img></a></div>
-         <div className="detail_ads_item "  key="2"><a href="https://mkvgames88.info/register.php?ref=FD1B18D28E" rel="noreferrer" target="_blank"><img alt="mkv" src="http://100.26.60.62/ipfs/QmPcJHpCDRGiZ45fxYN3Zf8JHXhSnEFsmEoFP1nGNQAwJB"></img></a></div>
+        <div className="detail_ads_item " key="1"><a href="https://jp888.store/?ref=studioskandal" rel="noreferrer" target="_blank"><img alt="jendralpoker" src="http://100.26.60.62/ipfs/QmYdRQH4nbGEJrsaQZGRpn87SYXZaaNr3cSGggL2usn7SB"></img></a></div>
+         <div className="detail_ads_item "  key="2"><a href="https://mkvgames88.info/register.php?ref=FD1B18D28E" rel="noreferrer" target="_blank"><img alt="mkv" src="http://100.26.60.62/ipfs/QmbGc8PHUS3qerfCGFTrk73vZvyuhKatP6bAiWujiSrc6e"></img></a></div>
       </div>
     )
 }
--- src/component/Contents/ads_top.js
+++ src/component/Contents/ads_top.js
@@ -5,7 +5,7 @@ function ads_top() {
     return (
         <div className="recomendedvideo_top_ads">
             <div className="top_ads_item " key="1"><a href="https://bb168.games/?ref=studios" rel="noreferrer" target="_blank"><img alt="bintangbola" src="http://100.26.60.62/ipfs/Qmc2WwaapsoY147GmuT2SqYX29v6hu53BPEAVWfig7NaoK"></img></a></div>
-            <div className="top_ads_item " key="2"><a href="https://iw888.store/Main.aspx?ref=275BE8" rel="noreferrer" target="_blank"><img alt="indowins" src="http://100.26.60.62/ipfs/QmQdTJHBmLkc9gdsi2zntbwo2c3oisjgrLmMRUs9C22ijz"></img></a></div>
+            <div className="top_ads_item " key="2"><a href="https://iw888.store/Main.aspx?ref=275BE8" rel="noreferrer" target="_blank"><img alt="indowins" src="http://100.26.60.62/ipfs/QmcSfTPJPjqc17D9VE9TiKpeSN44iZMdGdoSKajv2NJpMw"></img></a></div>
         </div>
     )
 }
