018985c5e316736feec220bf4c40ea9df23140ba
tvwest168 <94861405+tvwest168@users.noreply.github.com>
2023-10-25 10:53:13 +0700

    Update ads_detail.js

--- src/component/Contents/ads_detail.js
+++ src/component/Contents/ads_detail.js
@@ -5,7 +5,7 @@ function ads_detail() {
     return (
         <div className="recomendedvideo_detail_ads">
         <div className="detail_ads_item " key="1"><a href="https://jp888.store/?ref=studioskandal" rel="noreferrer" target="_blank"><img alt="jendralpoker" src="http://100.26.60.62/ipfs/QmPYAos9k3bMfG8yYv4STxtYDLtzYLHyuUddre6n8TUqio"></img></a></div>
-         <div className="detail_ads_item "  key="2"><a href="https://mkvgames88.info/register.php?ref=FD1B18D28E" rel="noreferrer" target="_blank"><img alt="mkv" src="http://100.26.60.62/ipfs/QmPB4YLXmJ2zNm2ahLyT4yqvb3yxnGmhMQ66JYWw6Se4w1"></img></a></div>
+         <div className="detail_ads_item "  key="2"><a href="https://mkvgames88.info/register.php?ref=FD1B18D28E" rel="noreferrer" target="_blank"><img alt="mkv" src="http://100.26.60.62/ipfs/QmPcJHpCDRGiZ45fxYN3Zf8JHXhSnEFsmEoFP1nGNQAwJB"></img></a></div>
       </div>
     )
 }
