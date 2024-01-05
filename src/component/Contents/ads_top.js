8e908b8578bb9caa6fa42b85f1e23774c90119e5
tvwest168 <94861405+tvwest168@users.noreply.github.com>
2024-01-05 08:22:39 +0700

    Update ads_top.js
    change to heylink

--- src/component/Contents/ads_top.js
+++ src/component/Contents/ads_top.js
@@ -4,8 +4,8 @@ import './ads_top.css';
 function ads_top() {
     return (
         <div className="recomendedvideo_top_ads">
-            <div className="top_ads_item " key="1"><a href="https://bb168.cloud/?ref=studios" rel="noreferrer" target="_blank"><img alt="bintangbola" src="http://100.26.60.62/ipfs/Qmc2WwaapsoY147GmuT2SqYX29v6hu53BPEAVWfig7NaoK"></img></a></div>
-            <div className="top_ads_item " key="2"><a href="https://iw888.cloud/Main.aspx?ref=275BE8" rel="noreferrer" target="_blank"><img alt="indowins" src="http://100.26.60.62/ipfs/QmcSfTPJPjqc17D9VE9TiKpeSN44iZMdGdoSKajv2NJpMw"></img></a></div>
+            <div className="top_ads_item " key="1"><a href="https://heylink.me/bintangbola303/" rel="noreferrer" target="_blank"><img alt="bintangbola" src="http://100.26.60.62/ipfs/Qmc2WwaapsoY147GmuT2SqYX29v6hu53BPEAVWfig7NaoK"></img></a></div>
+            <div className="top_ads_item " key="2"><a href="https://heylink.me/indowins303" rel="noreferrer" target="_blank"><img alt="indowins" src="http://100.26.60.62/ipfs/QmcSfTPJPjqc17D9VE9TiKpeSN44iZMdGdoSKajv2NJpMw"></img></a></div>
         </div>
     )
 }
