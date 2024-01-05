3742cbb637941b63a1123384b64c5484c26d193f
tvwest168 <94861405+tvwest168@users.noreply.github.com>
2024-01-05 08:20:48 +0700

    Update ads_bottom.js
    change to heylink

--- src/component/Contents/ads_bottom.js
+++ src/component/Contents/ads_bottom.js
@@ -4,8 +4,8 @@ import './ads_bottom.css';
 function ads_bottom() {
     return (
         <div className="recomendedvideo_bottom_ads">
-           <div className="bottom_ads_item " key="1"><a href="https://mkvgames88.info/register.php?ref=FD1B18D28E" rel="noreferrer" target="_blank"><img alt="mkvgames" src="http://100.26.60.62/ipfs/QmbGc8PHUS3qerfCGFTrk73vZvyuhKatP6bAiWujiSrc6e"></img></a></div>
-            <div className="bottom_ads_item " key="2"><a href="https://jp888.store/?ref=studioskandal" rel="noreferrer" target="_blank"><img alt="jendralpoker" src="http://100.26.60.62/ipfs/QmYdRQH4nbGEJrsaQZGRpn87SYXZaaNr3cSGggL2usn7SB"></img></a></div>
+           <div className="bottom_ads_item " key="1"><a href="https://heylink.me//mkvgames303" rel="noreferrer" target="_blank"><img alt="mkvgames" src="http://100.26.60.62/ipfs/QmbGc8PHUS3qerfCGFTrk73vZvyuhKatP6bAiWujiSrc6e"></img></a></div>
+            <div className="bottom_ads_item " key="2"><a href="https://heylink.me/jendralpoker303" rel="noreferrer" target="_blank"><img alt="jendralpoker" src="http://100.26.60.62/ipfs/QmYdRQH4nbGEJrsaQZGRpn87SYXZaaNr3cSGggL2usn7SB"></img></a></div>
          </div>
     )
 }
